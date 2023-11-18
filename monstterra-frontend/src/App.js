4b22a15c224e2d4340d5cff4bc3b4d6af6c39a15
Juno9170 <ryanzhuturtle@gmail.com>
2023-11-18 11:25:58 -0500

    First iteration

--- monstterra-frontend/src/App.js
+++ monstterra-frontend/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
   return (
-
-  );
+    <div>
+      <h1>HIIIIIIIIIIIIII</h1>
+      <h2>TESTING</h2>
+    </div>
+  )
 }
 
-export default App;
+export default App
