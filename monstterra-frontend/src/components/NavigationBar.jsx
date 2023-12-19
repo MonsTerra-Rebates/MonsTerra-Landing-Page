000f128e2ea170c188ba744ab70efb139f585bcd
Juno9170 <ryanzhuturtle@gmail.com>
2023-12-18 20:54:11 -0500

    Update 2
    added some styles (fonts, icons)

--- monstterra-frontend/src/components/NavigationBar.jsx
+++ monstterra-frontend/src/components/NavigationBar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Show, Hide } from '@chakra-ui/react'
+import {HamburgerIcon} from '@chakra-ui/icons'
 import './NavigationBarCss.css' 
 const NavigationBar = () =>{
     return (
         <div className="navbar-container">
-            <div>Logo</div>
+            
+            <div className="logo-container">Logo</div>
+
+            <Hide breakpoint='(max-width: 600px)'>
+            <div className="redirects-contianer">
             <div>Redirect 1</div>
             <div>Redirect 2</div>
             <div>Redirect 3</div>
+            </div>
+            </Hide>
+
+            <Show breakpoint='(max-width: 600px)'>
+                <div>
+                    <HamburgerIcon boxSize={30}/>
+                </div>
+            </Show >
         </div>
     )
 }
